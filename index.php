73b7656a40169a84c054f7d46e666a30f0093ca8
Sombit Pramanik <m.sombitpramanik@gmail.com>
2023-10-31 21:52:31 +0530

    add the basic structure for the starting code and created the sturcture for starting

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+require 'config.php';
+require 'phpqrcode-2010100721_1.1.4/phpqrcode/qrlib.php';
+// Include the Default HTML structure 
+include 'HTML/html_start.html';
+// Actual code for the Website
+
+
+// Include the HTML default end
+include 'HTML/html_end.html';
+?>
