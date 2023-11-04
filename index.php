7fa7de1cc42d401167cf30c744e6493931405597
Sombit Pramanik <m.sombitpramanik@gmail.com>
2023-11-04 09:34:20 +0530

    compleate the base structure and the compleate the left menu

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 require 'config.php';
 require 'phpqrcode-2010100721_1.1.4/phpqrcode/qrlib.php';
-// Include the Default HTML structure 
+// Include the Default HTML structure Start
 include 'HTML/html_start.html';
 // Actual code for the Website
-
+include 'HTML/home.html';   // IT Refers to the Body of the HTML
 
 // Include the HTML default end
 include 'HTML/html_end.html';
