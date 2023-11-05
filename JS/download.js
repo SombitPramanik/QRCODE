37c40dd563f6934a19686e7af245d1484947a53c
Sombit Pramanik <m.sombitpramanik@gmail.com>
2023-11-05 09:37:11 +0530

    finish up the dark thema with download functonality

---
+++ JS/download.js
@@ -0,0 +1,12 @@
+const downloadButton = document.getElementById('downloadButton');
+const image = document.getElementById('myImage');
+
+downloadButton.addEventListener('click', () => {
+  const a = document.createElement('a');
+  a.href = image.src;
+  a.download = 'downloaded_image.jpg'; // Specify the download file name
+  a.style.display = 'none'; // Hide the anchor element
+  document.body.appendChild(a);
+  a.click(); // Trigger a click event on the anchor element
+  document.body.removeChild(a); // Clean up the anchor element
+});
